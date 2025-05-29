98dc263d8c65b4e5b62f459dcfb3395895f248ff
Cyknow <cyknowtechservnigltd@gmail.com>
2025-05-29 15:22:17 +0100

    commit

---
+++ src/Components/Hero/Hero.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div>
+      <section className="hero-section">
+      <h3>we all love</h3>
+    <h1>nature</h1>
+    <p>Look deep into nature, and you will
+      understand everything better.</p>
+    <button>Get Started</button> 
+  </section>
+    </div>
+  )
+}
+
+export default Hero
